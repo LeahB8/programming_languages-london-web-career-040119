f2024abf281bea4b5cb36e7618da6fb065436ebd
Leah Boyd <leahboyd08@gmail.com>
2019-03-27 17:53:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,39 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do | key, value |
+    
+    end    
+  end
+  return new_hash
 end
