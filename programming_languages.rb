1e393e30fd6294571fddb932f5ee3b03b12bf530
Leah Boyd <leahboyd08@gmail.com>
2019-03-27 19:21:35 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -31,9 +31,31 @@ languages = {
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do | key, value |
-    
+  
+  languages.each do | style, description |
+    description.each do | language, type |
+      if new_hash.has_key?(language)
+        new_hash[language][:style] << style
+      else
     end    
   end
   return new_hash
+end
+
+
+def reformat_languages(languages)
+  new_hash = {} 
+
+  languages.each do |style, description|
+    description.each do |lang, type|
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
+      end
+    end
+  end
+
+  new_hash  
 end
